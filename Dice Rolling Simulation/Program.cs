40e3e8110772b136dc122a29da88217c9739184f
victorj1 <username@users.noreply.github.com>
2016-07-24 23:53:17 -0400

    Add project files.

---
+++ Dice Rolling Simulation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice_Rolling_Simulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
